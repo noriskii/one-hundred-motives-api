029359404f392c3eee6325eb0be8d3b8fb9490b0
Lucas Augusto <luckaugusto@gmail.com>
2020-06-26 03:50:54 -0300

    new hello

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const MotiveController = require('./controllers/MotiveController')
 
 const routes = express.Router()
 
+routes.get("/", (req, res) => { return res.send('<h1>Hello World</h1>') })
 routes.get("/motives", MotiveController.index)
 routes.get("/motive/:motiveNumber", MotiveController.show)
 
